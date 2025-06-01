a395966b2c7c360684fb7eae510b1cf70bba7b4a
marshuni <1261565515@outlook.com>
2025-06-01 18:27:46 +0800

    将baseline从ResNet50改为34；跑通代码

--- model/baseline.py
+++ model/baseline.py
@@ -1,22 +1,22 @@
 import torch
 import torch.nn as nn
 import torchvision
-from torchvision.models import ResNet50_Weights
+from torchvision.models import ResNet34_Weights
 
-# 定义ResNet50模型
-class ResNet50Classifier(nn.Module):
-    def __init__(self, num_classes=10, weights=ResNet50_Weights.DEFAULT):
-        super(ResNet50Classifier, self).__init__()
-        # 加载预训练的ResNet50模型
-        self.resnet50 = torchvision.models.resnet50(weights=weights)
+# 定义ResNet34模型
+class ResNet34Classifier(nn.Module):
+    def __init__(self, num_classes=10, weights=ResNet34_Weights.DEFAULT):
+        super(ResNet34Classifier, self).__init__()
+        # 加载预训练的ResNet34模型
+        self.resnet34 = torchvision.models.resnet34(weights=weights)
         
         # 冻结所有层（如果只想训练分类层）
-        # for param in self.resnet50.parameters():
+        # for param in self.resnet34.parameters():
         #     param.requires_grad = False
             
         # 替换最后的全连接层以适应我们的分类任务
-        num_features = self.resnet50.fc.in_features
-        self.resnet50.fc = nn.Linear(num_features, num_classes)
+        num_features = self.resnet34.fc.in_features
+        self.resnet34.fc = nn.Linear(num_features, num_classes)
         
     def forward(self, x):
-        return self.resnet50(x)
+        return self.resnet34(x)
